683f1ab9b30ab7074bef0809e7964abcf93dbc96
JPBG-USP <joao_garciajp@usp.br>
2025-10-29 16:23:47 -0300

    Adding GPIO definitions

---
+++ lib/GraspeGPIO/GraspeGPIO.h
@@ -0,0 +1,28 @@
+#ifndef _GRASPE_GPIO_H_
+#define _GRASPE_GPIO_H_
+
+// GPIO Pin definitions
+#define GRASPE_GPIO_VERSION "3.1.0"
+
+// LEDs pins
+#define LED_ERROR_PIN 27
+#define LED_WARNING_PIN 12
+#define LED_SUCCESS_PIN 13
+
+// Motor control pins
+#define MOTOR1_PIN_A 32
+#define MOTOR1_PIN_B 33
+#define MOTOR2_PIN_A 26
+#define MOTOR2_PIN_B 25
+#define MOTOR3_PIN_A 4
+#define MOTOR3_PIN_B 18
+#define MOTOR4_PIN_A 19
+#define MOTOR4_PIN_B 23
+
+// Motor Encoder pins
+#define MOTOR1_ENCODER_PIN 36
+#define MOTOR2_ENCODER_PIN 39
+#define MOTOR3_ENCODER_PIN 34
+#define MOTOR4_ENCODER_PIN 35
+
+#endif // _GRASPE_GPIO_H_
