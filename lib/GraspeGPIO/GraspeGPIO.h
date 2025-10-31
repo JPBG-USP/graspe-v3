822b832352d6ede1437d5299b5d612e6de426e34
JPBG-USP <joao_garciajp@usp.br>
2025-10-31 20:10:58 -0300

    Adding function to indicate status on the LEDs
    Now comprising the status display on the LEDs in one function

--- lib/GraspeGPIO/GraspeGPIO.h
+++ lib/GraspeGPIO/GraspeGPIO.h
@@ -32,6 +32,16 @@ namespace GraspeGPIO {
         pinMode(LED_WARNING_PIN, OUTPUT);
         pinMode(LED_SUCCESS_PIN, OUTPUT);
     };
+
+    inline void indicateStatus(bool error, bool warning, bool success){
+        digitalWrite(LED_ERROR_PIN, error ? HIGH : LOW);
+        digitalWrite(LED_WARNING_PIN, warning ? HIGH : LOW);
+        digitalWrite(LED_SUCCESS_PIN, success ? HIGH : LOW);
+    };
+
+    inline const char* getVersion() {
+        return GRASPE_GPIO_VERSION;
+    };
 }
 
 #endif // _GRASPE_GPIO_H_
